e3e1d4a2690c302c3992af8f67dacf138fd0bdf4
lukaszk960214 <212314@edu.p.lodz.pl>
2019-12-15 12:59:14 +0100

    Update ProjektZPI.java

--- src/ProjektZPI.java
+++ src/ProjektZPI.java
@@ -4,7 +4,7 @@ public class ProjektZPI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Group Name: Turbo_Bigos\nTeam Leader: Dawid Jany\tTachulek");
+		System.out.println("Group Name: Turbo_Bigos\nTeam Leader: Dawid Jany\tTachulek\n Tester : Marcin Bugaj MarcinBpl");
 	}
 
 }
