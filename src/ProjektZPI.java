7bb99727f5a0b02d0e1e86ceb181482ef450f7dd
Dawid Jany <Dawid.Jany@Harman.com>
2019-12-15 12:15:30 +0100

    First and last commit for Release 1.0.0 version

---
+++ src/ProjektZPI.java
@@ -0,0 +1,10 @@
+
+public class ProjektZPI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Group Name: Turbo_Bigos\nTeam Leader: Dawid Jany\tTachulek");
+	}
+
+}
