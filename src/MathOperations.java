97a0eb106b9609bce98a20b779c3194597053145
Marcin <materacbr@gmail.com>
2020-01-12 09:19:48 +0100

    Add multiplication

--- src/MathOperations.java
+++ src/MathOperations.java
@@ -1,25 +1,10 @@
-public class MathOperations implements IArithmeticsAdd, IArithmeticsDiv, IArithmeticsDiff {
 
-	public boolean difference(double a, double b)
+public class MathOperations implements IArithmeticsMulti {
+	
+	
+	public double multiplication(double a, double b)
 	{
-		if(a != b)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-   }
-   
-	public double addition(double a, double b)
-	{
-		return a+b;
-  }
-  
-	public double division(double a, double b)
-	{
-		return (double)(a/b);
+		return a*b;
 	}
 
 }
