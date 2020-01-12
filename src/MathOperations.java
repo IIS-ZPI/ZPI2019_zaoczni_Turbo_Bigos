b7200cfa6e407d94abaac5b1a5cb0cce53422dc8
Marcin <materacbr@gmail.com>
2020-01-12 09:36:13 +0100

    Update MathOperations.java

--- src/MathOperations.java
+++ src/MathOperations.java
@@ -1,10 +1,29 @@
-
-public class MathOperations implements IArithmeticsMulti {
-	
-	
+public class MathOperations implements IArithmeticsAdd, IArithmeticsDiv, IArithmeticsDiff, IArithmeticsMulti 
+{
 	public double multiplication(double a, double b)
 	{
 		return a*b;
 	}
 
-}
+	public boolean diffrence (double a, double b)
+	{
+		if (a != b)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public double addition(double a, double b)
+	{
+		return a+b;
+	}
+
+	public double division(double a, double b)
+	{
+		return (double)(a/b);
+	}
+} 
