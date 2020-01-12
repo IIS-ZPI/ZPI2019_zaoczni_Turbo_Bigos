5cf1943c4ed8c7ebcddbd2f3f96a36767c49dcd7
Marcin <materacbr@gmail.com>
2020-01-12 09:36:13 +0100

    Update MathOperations.java

--- src/MathOperations.java
+++ src/MathOperations.java
@@ -1,12 +1,31 @@
+public class MathOperations implements IArithmeticsAdd, IArithmeticsDiv, IArithmeticsDiff, IArithmeticsMulti 
+{
+	public double multiplication(double a, double b)
+	{
+		return a*b;
+	}
 
-public class MathOperations implements IArithmeticsDiv {
-	
-	
+	public boolean diffrence (double a, double b)
+	{
+		if (a != b)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public double addition(double a, double b)
+	{
+		return a+b;
+	}
+
 	public double division(double a, double b)
 	{
 		return (double)(a/b);
 	}
-
-}
+} 
 // wszystko wydaje sie byc OK
 //komentarz seba zad 6
