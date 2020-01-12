93f7105903812f23f02e87b451b16d4d37e9f72c
kitazdw <kitazdw@gmail.com>
2020-01-12 09:01:35 +0100

    Diff implementation

--- src/MathOperations.java
+++ src/MathOperations.java
@@ -1,7 +1,17 @@
 
-public class MathOperations {
+public class MathOperations implements IArithmeticsDiff {
 	
 	
-	
+	public boolean difference(double a, double b)
+	{
+		if(a != b)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
 }
