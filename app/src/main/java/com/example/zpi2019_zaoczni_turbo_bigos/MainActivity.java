5fb2ccbb063fcc261a0951d4901ff62b4a159da9
SPSzklarek212494 <56228173+SPSzklarek212494@users.noreply.github.com>
2020-02-15 11:35:11 +0100

    zmiana rodzaju głównego activity

---
+++ app/src/main/java/com/example/zpi2019_zaoczni_turbo_bigos/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.zpi2019_zaoczni_turbo_bigos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
